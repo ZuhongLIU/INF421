72edd0c56a5207aff9980d8c401a6ae8f24bb508
XiaoxianYang <114261696+XiaoxianYang@users.noreply.github.com>
2023-01-20 00:07:28 +0100

    Update Utils.java

--- src/Utils.java
+++ src/Utils.java
@@ -3,7 +3,9 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Stream;
-
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.BufferedWriter;
 public class Utils {
 	
 	static String Onehot2String(int[]label) {
@@ -19,26 +21,33 @@ public class Utils {
 	}
 	
    void testReadFile2(String fileName) throws IOException {
-   // Useless Function， just for I/O test 
-	   
-   // 读取文件内容到Stream流中，按行读取
-   //List<String> lines = Files.readAllLines(Paths.get(fileName));
-   //System.out.println(lines.findFirst().get());
-   // 随机行顺序进行数据处理
-   //lines.forEach(ele -> {
-   //   System.out.println(ele);
-   //});
+
    return;
 }
+
+   public class Write {
+       public static void write(String file_path,String word) throws IOException {
+           FileOutputStream fileOutputStream = null;
+           if(word!="\r\n") {
+        	   word = word + ' ';
+           }
+           File file = new File(file_path);
+           if(!file.exists()){
+               file.createNewFile();
+           }
+           fileOutputStream = new FileOutputStream(file,true);
+           fileOutputStream.write((word).getBytes("gbk"));
+//           fileOutputStream.flush();
+           fileOutputStream.close();
+       }
+   }
+   
    public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 	   String fileName = "C:\\Users\\lc010\\eclipse-workspace\\INF421\\input\\0.in";
 	   Utils utils=new Utils();
 	   utils.testReadFile2(fileName);
-	   
-	   
-
 	}
-
+}
 
 }
